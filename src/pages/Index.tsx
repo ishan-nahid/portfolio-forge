ff000a8228548173357ff8ba4b67e6c930e61538
Ishan Ahmad <nahidboss96@gmail.com>
2026-02-28 16:19:51 +0600

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -82,7 +82,7 @@ type OpenSourceData = {
   date: string;
 };
 
-type SkillData = { id: string; name: string; category: string context?: string};
+type SkillData = { id: string; name: string; category: string; context?: string };
 type EducationData = { id: string; degree: string; institution: string; start_date: string; grade?: string; end_date: string; description: string; period?: string };
 type CertificationData = { id: string; title: string; issuer: string; date_earned: string; credential_id: string };
 type HonorData = { id: string; title: string; issuer: string; date_received: string; description: string };
