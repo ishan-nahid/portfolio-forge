527c2f4c0b4a108923037a0db150c66676f838cf
Ishan Ahmad <nahidboss96@gmail.com>
2026-03-01 01:40:24 +0600

    Create use-theme.ts

---
+++ src/hooks/use-theme.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export function useTheme() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove("light", "dark");
+    root.classList.add(theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return { theme, toggleTheme };
+}
